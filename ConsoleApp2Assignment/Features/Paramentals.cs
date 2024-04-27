dbd24e1814e061d6621e3f2a3d8eeaba7d73a30d
John Precious Umukoro <mcpreciousshalom@hotmail.com>
2024-04-27 05:12:15 -0700

    Add project files.

---
+++ ConsoleApp2Assignment/Features/Paramentals.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2Assignment.Features
+{
+    internal class Paramentals
+    {
+        int X;
+        int Y;
+        public Paramentals(int a, int b) 
+        {
+
+            X = a; 
+            
+            Y = b;
+        
+        
+        }
+        
+
+        public  void Results() 
+        {
+
+            Console.WriteLine(X+Y);
+
+        }
+
+
+
+        //assigment 2 using paramentals
+
+
+        string Username= "first name,  Last name";
+        
+        public Paramentals(string username)// constructor
+        {
+
+            Username=username;
+
+
+        }
+
+
+        public void Redisplay()
+        {
+
+            Console.WriteLine(Username);
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+}
---
+++ ConsoleApp2Assignment/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp2Assignment.Features;
+using System.Linq.Expressions;
+
+Console.WriteLine("ASSIGNment Result. printing out even numbers from 1-50!");
+
+//Assinngment1
+// Use (For) to print out even number from 1-50
+
+for (int Y = 1; Y <= 50; Y++)
+{
+  
+    if (Y % 2 == 0)
+
+       Console.WriteLine("Y = " + Y);
+    
+
+
+}
+
+//Assignment 2
+
+Console.WriteLine("assignment ii. using switch case to print out any day of the week");
+//Switch Case
+string Nameofday = "Friday";
+Console.WriteLine("please enter W as week");
+
+Console.ReadLine();
+
+switch (Nameofday)
+{
+    case "Monday":
+        {
+            Console.WriteLine(" Monday");
+            break;
+        }
+    case "Tuesday":
+        {
+
+            Console.WriteLine("Tuesday");
+            break;
+        }
+    case "Friday":
+        {
+
+            Console.WriteLine("Friday");
+            break;
+
+        }
+    default:
+        {
+            Console.WriteLine("name not found");
+            break;
+        }
+
+}
+
+/*Assignment 3. create a class that will have constructor while using string paramental value in the constructor.*/
+
+
+Paramentals value = new Paramentals(3, 6); //instance of a class
+value.Results();
+
+
+Paramentals Rresult= new Paramentals( "Ivan, Precious"); //instance of the class Paramentals
+Rresult.Redisplay();
+
+Console.ReadKey();
